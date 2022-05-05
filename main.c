72ec8f11ab31f35d794483f331b615bad03118e7
baindfrst <n11174@yandex.ru>
2022-05-05 16:19:29 +0300

    pass

--- main.c
+++ main.c
@@ -4,29 +4,48 @@
 int main(){
 	printf("*Выбор седьмого дозвонившегося слушателя*\n");
 	int i;
+	int backup = 0;
 	node* Call = NULL;
-	char a = 'a';
+	node* Back = NULL;
+	int a;
 	for (i = 0; i < 3; i++){
 		pushQ(a, &Call);
 		a++;
 	}
 	printf("Первые дозвонившиеся:\n");
 	printQ(Call);
-	printf("\nДобавление людей в очередь:");
-	while (lenQ(Call) < 7){
-		pushQ(a, &Call);
-		printf("\nДозвонился ");
-		printf("%c", lastQ(Call)->elem);
-		a++;
+	scanf("%s", a);
+	while ((char)a != 'k')
+	{
+		if ((char)a == 'i')
+		{
+			pushQ((char)(a % 256), &Call);
+			printf("ввели %c\n", (char)(a % 256));
+		}
+		if ((char)a == 'o')
+		{
+			popQ(&Call);
+			printf("закончил работу: %c\n", (char)(a % 256));
+		}
+		if ((char)a == 'c')
+		{
+			if (peakQ(Call, (char)(a % 256)))
+			{
+				printf("%c в очереди", (char)(a % 256));
+			}
+			else
+			{
+				printf("%c не в очереди", (char)(a % 256));
+			}
+		}
+		backup++;
+		if (backup == 2)
+		{
+			copyQ(&Back, Call);
+			printf("сохранение произошло\n");
+			backup = 0;
+		}
 	}
-	printf("\nОчередь: ");
-	printQ(Call);
-	printf("\nУдаление из очереди всех, кроме седьмого:\n");
-	do{
-		printf("Отключается ");
-		printf("%c", popQ(&Call));
-		printf("\n");
-	} while (Call != lastQ(Call));	
 	printf("Седьмой дозвонившийся слушатель: ");
 	printQ(Call);
 	popQ(&Call);
