2bd00296ff0145bf995c9ae653ab01ad4daf29fc
baindfrst <n11174@yandex.ru>
2022-05-05 08:50:29 +0300

    pass

--- main.c
+++ main.c
@@ -8,13 +8,14 @@ int main()
     pushQ('a', &head);
     pushQ('b', &head);
     pushQ('c', &head);
-    pushQ('d', &head);
-    pushQ('e', &head);
     pushQ('f', &head);
+    pushQ('e', &head);
+    pushQ('d', &head);
     printQ(head);
     printf("\n");
     node* new = NULL;
     copyQ(&new, head);
+    sortQ(new);
     printQ(new);
     return 0;
 }
