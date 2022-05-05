b9263afadfec078ef29f7cf600743953e1c5f4d9
baindfrst <n11174@yandex.ru>
2022-05-05 09:38:50 +0300

    dem

--- main.c
+++ main.c
@@ -5,17 +5,43 @@
 int main()
 {
     node* head = NULL;
-    pushQ('a', &head);
-    pushQ('b', &head);
-    pushQ('c', &head);
-    pushQ('f', &head);
-    pushQ('e', &head);
-    pushQ('d', &head);
+    node* new = NULL;
+    char c;
+    for (int i = 0; i != 6; i++)
+    {
+        scanf("%c", &c);
+        pushQ(c, &head);
+    }
+    pushQ('l', &head);
     printQ(head);
     printf("\n");
-    node* new = NULL;
+    for (int i = 0; i != lenQ(head) - 1; i++)
+    {
+        c = popQ(&head);
+        printf("%c ", c);
+    }
+    printf("\n");
     copyQ(&new, head);
     sortQ(new);
     printQ(new);
+    printf("was sorted\n");
+    if (check_copyQ(head, new))
+    {
+        printf("ввод был отсортированный\n");
+    }
+    else
+    {
+        printf("ввод был не отсортированный\n");
+    }
+    freeQ(head);
+    freeQ(new);
+    if (emptyQ(new) && emptyQ(head))
+    {
+        printf("end fine\n");
+    }
+    else
+    {
+        printf("clear data\n");
+    }
     return 0;
 }
